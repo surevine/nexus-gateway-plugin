2da7b98eb8714bc168b73fed3d72cceea669ea64
Rich Midwinter <rich.midwinter@gmail.com>
2014-02-25 11:30:39 +0000

    Fix for sending artifact on updated security label.

--- src/main/java/com/surevine/community/nexus/OnUploadWebFilter.java
+++ src/main/java/com/surevine/community/nexus/OnUploadWebFilter.java
@@ -21,7 +21,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.logging.Log;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
@@ -43,8 +42,7 @@ public class OnUploadWebFilter implements Filter {
 			".sha1",
 			".md5",
 			".pom",
-			"/maven-metadata.xml",
-			"-securitylabel.xml"
+			"maven-metadata.xml"
 	};
 
 	public void doFilter(ServletRequest req, ServletResponse res,
@@ -54,117 +52,160 @@ public class OnUploadWebFilter implements Filter {
 		final HttpServletRequest request = (HttpServletRequest) req;
 		if ("PUT".equals(request.getMethod()) && request.getRequestURI().startsWith(REPOSITORIES)) {
 			final String path = request.getRequestURI().substring(REPOSITORIES.length());
+			final Path artifact = Paths.get(System.getProperty("nexus-work"), "storage", path);
 			
-			if (notEndsWithAny(path, IGNORED_EXTENSIONS)) {
-		    	final String[] repositories = NexusGatewayProperties.get(
-		    			NexusGatewayProperties.NEXUS_REPOSITORIES).split(",");
-		    	  
-				final Path artifact = Paths.get(System.getProperty("nexus-work"), "storage", path);
-				final String artifactStr = artifact.toString();
-				final String repositoryTmp = artifactStr.substring(artifactStr.indexOf("/storage/") +9);
-				final String repository = repositoryTmp.substring(0, repositoryTmp.indexOf("/"));
+			if (path.endsWith("-securitylabel.xml")) {
+				// Find and send to gateway all non-pom/hash/metadata.xml files of their most recent version.
 				
-				// Only pick up changes for defined repositories.
-				if (Arrays.asList(repositories).contains(repository)) {
-					try {
-						final File[] labelPaths = artifact.getParent().toFile().listFiles(new FilenameFilter() {
-							public boolean accept(File dir, String name) {
-								return name.endsWith("-securitylabel.xml");
-							}
-						});
-						
-						if (labelPaths.length != 1) {
-							System.out.println("Uploaded artifact has " +labelPaths.length +" securitylabel.xml files. Ignoring.");
-							return; // Warning. Return statement in middle of method!
-						}
-						
-						final Path labelPath = labelPaths[0].toPath();
-						
-						final Path pomPath = artifact.getParent().toFile().listFiles(new FilenameFilter() {
-							public boolean accept(File dir, String name) {
-								return name.endsWith(".pom");
-							}
-						})[0].toPath();
-						
-						// Read metadata
-						final String labelXml = Joiner.on("").join(Files.readAllLines(labelPath, Charset.forName("UTF-8")));
-						final SecurityLabel label = new SecurityLabel(labelXml);
-						
-						// Read pom information
-						final String pomXml = Joiner.on("").join(Files.readAllLines(pomPath, Charset.forName("UTF-8")));
-						final Pom pom = new Pom(pomXml);
-						
-						final Map<String, String> properties = new HashMap<String, String>();
-						properties.put("groupId", pom.getGroupId());
-						properties.put("artifactId", pom.getArtifactId());
-						properties.put("version", pom.getVersion());
-						properties.put("packaging", pom.getPackaging());
-						properties.put("classification", label.getClassification());
-						properties.put("decorator", label.getDecorator());
-						properties.put("groups", Joiner.on(",").join(label.getGroups()));
-						properties.put("countries", Joiner.on(",").join(label.getCountries()));
-						properties.put("source_type", "NEXUS");
-						properties.put("repository", repository);
-						properties.put("name", artifact.getFileName().toString());
-						
-						final StringBuilder metadata = new StringBuilder();
-						metadata.append("{");
-						metadata.append(String.format("\"repository\": \"%s\",", properties.get("repository")));
-						metadata.append(String.format("\"groupId\": \"%s\",", properties.get("groupId")));
-						metadata.append(String.format("\"artifactId\": \"%s\",", properties.get("artifactId")));
-						metadata.append(String.format("\"version\": \"%s\",", properties.get("version")));
-						metadata.append(String.format("\"packaging\": \"%s\",", properties.get("packaging")));
-						metadata.append(String.format("\"classification\": \"%s\",", properties.get("classification")));
-						metadata.append(String.format("\"decorator\": \"%s\",", properties.get("decorator")));
-						metadata.append(String.format("\"groups\": \"%s\",", properties.get("groups")));
-						metadata.append(String.format("\"countries\": \"%s\",", properties.get("countries")));
-						metadata.append(String.format("\"name\": \"%s\",", properties.get("name")));
-						metadata.append("\"source_type\": \"NEXUS\"");
-						metadata.append("}");
-						
-						System.out.println("Metadata: " +metadata.toString());
-						
-						// Create tar.gz in temporary directory
-						final Path tmp = Paths.get("/tmp/nexus-upload", UUID.randomUUID().toString());
-						final Path tarFile = Paths.get(tmp.toString(), tmp.getFileName().toString() +".tar.gz");
-						final Path metadataPath = Paths.get(tmp.toString(), ".metadata.json");
-						final Path artifactPath = Paths.get(tmp.toString(), artifact.getFileName().toString());
-						
-						Files.createDirectories(tmp);
-						Files.copy(artifact, artifactPath);
-						Files.write(metadataPath, metadata.toString().getBytes(Charset.forName("UTF-8")));
-		
-						// Tarball.
-						try {
-							Runtime.getRuntime().exec(
-									new String[] {"tar", "cvzf", tarFile.toString(),
-											metadataPath.getFileName().toString(),
-											artifactPath.getFileName().toString()},
-									new String[] {},
-									tmp.toFile()).waitFor();
-						} catch (InterruptedException e) {
-							e.printStackTrace(); // FIXME: Handle better.
-						}
-						
-						// Post to gateway
-						// FIXME: Insanely hard coded.
-						pushToGeneric(tmp, tmp.getFileName().toString(), properties);
-						
-						// Cleanup
-						/*Files.delete(Paths.get(tmp.toString(), ".metadata.json"));
-						Files.delete(Paths.get(tmp.toString(), artifact.getFileName().toString()));
-						Files.delete(Paths.get(tmp.toString(), tarFile.toString());
-						Files.delete(tmp);*/
-					} catch (Exception e) {
-						System.out.println(e.getMessage());
-						e.printStackTrace();
-						throw new RuntimeException(e);
+				// List all sendable files (not maven-metadata.xml, sha1, md5, -security-label.xml, etc)
+				final String[] artifacts = artifact.getParent().toFile().list(new FilenameFilter() {
+					@Override
+					public boolean accept(final File dir, final String name) {
+						return !endsWithAny(name, IGNORED_EXTENSIONS) && !name.endsWith("-securitylabel.xml");
 					}
-				} else {
-					System.out.println("Ignoring artifact destined for " +repository +" repository. Not found in +" +Joiner.on(",").join(repositories));
+				});
+				
+				final Map<String, String> groupByExtension = new HashMap<>();
+				for (final String filename : artifacts) {
+					final String extension = filename.substring(filename.lastIndexOf('.'));
+					if (groupByExtension.containsKey(extension)) {
+						if (groupByExtension.get(extension).compareTo(filename) > 1) {
+							groupByExtension.put(extension, filename);
+						}
+					} else {
+						groupByExtension.put(extension, filename);
+					}
 				}
+				
+				for (final String key : groupByExtension.keySet()) {
+					final Path guessedArtifact = Paths.get(artifact.getParent().toString(), groupByExtension.get(key));
+					sendFile(guessedArtifact);
+				}
+			} else if (!endsWithAny(path, IGNORED_EXTENSIONS)) {
+				sendFile(artifact);
 			}
 		}
+	}
+	
+	private void sendFile(final Path artifact) {
+		System.out.println("Processing artifact: " +artifact);
+		
+    	final String[] repositories = NexusGatewayProperties.get(
+    			NexusGatewayProperties.NEXUS_REPOSITORIES).split(",");
+    	
+		final String artifactStr = artifact.toString();
+		final String repositoryTmp = artifactStr.substring(artifactStr.indexOf("/storage/") +9);
+		final String repository = repositoryTmp.substring(0, repositoryTmp.indexOf("/"));
+		
+		// Only pick up changes for defined repositories.
+		if (Arrays.asList(repositories).contains(repository)) {
+			try {
+				final File[] labelPaths = artifact.getParent().toFile().listFiles(new FilenameFilter() {
+					public boolean accept(File dir, String name) {
+						return name.endsWith("-securitylabel.xml");
+					}
+				});
+				
+				Arrays.sort(labelPaths);
+				
+				if (labelPaths.length == 0) {
+					System.out.println("Uploaded artifact has no security label. Ignoring.");
+					return; // Warning. Return statement in middle of method!
+				}
+				
+				// Take the last of the sorted label paths. Maybe this is correct.
+				final Path labelPath = labelPaths[labelPaths.length - 1].toPath();
+				
+				final File[] pomFiles = artifact.getParent().toFile().listFiles(new FilenameFilter() {
+					public boolean accept(File dir, String name) {
+						return name.endsWith(".pom");
+					}
+				});
+				
+				if (pomFiles.length == 0) {
+					System.out.println("Ack. No pom found. Sssh...");
+					return; // Warning. Return statement in middle of method!
+				}
+				
+				final Path pomPath = pomFiles[0].toPath();
+				
+				// Read metadata
+				final String labelXml = Joiner.on("").join(Files.readAllLines(labelPath, Charset.forName("UTF-8")));
+				final SecurityLabel label = new SecurityLabel(labelXml);
+				
+				// Read pom information
+				final String pomXml = Joiner.on("").join(Files.readAllLines(pomPath, Charset.forName("UTF-8")));
+				final Pom pom = new Pom(pomXml);
+				
+				final Map<String, String> properties = new HashMap<String, String>();
+				properties.put("groupId", pom.getGroupId());
+				properties.put("artifactId", pom.getArtifactId());
+				properties.put("version", pom.getVersion());
+				properties.put("packaging", pom.getPackaging());
+				properties.put("classification", label.getClassification());
+				properties.put("decorator", label.getDecorator());
+				properties.put("groups", Joiner.on(",").join(label.getGroups()));
+				properties.put("countries", Joiner.on(",").join(label.getCountries()));
+				properties.put("source_type", "NEXUS");
+				properties.put("repository", repository);
+				properties.put("name", artifact.getFileName().toString());
+				
+				final StringBuilder metadata = new StringBuilder();
+				metadata.append("{");
+				metadata.append(String.format("\"repository\": \"%s\",", properties.get("repository")));
+				metadata.append(String.format("\"groupId\": \"%s\",", properties.get("groupId")));
+				metadata.append(String.format("\"artifactId\": \"%s\",", properties.get("artifactId")));
+				metadata.append(String.format("\"version\": \"%s\",", properties.get("version")));
+				metadata.append(String.format("\"packaging\": \"%s\",", properties.get("packaging")));
+				metadata.append(String.format("\"classification\": \"%s\",", properties.get("classification")));
+				metadata.append(String.format("\"decorator\": \"%s\",", properties.get("decorator")));
+				metadata.append(String.format("\"groups\": \"%s\",", properties.get("groups")));
+				metadata.append(String.format("\"countries\": \"%s\",", properties.get("countries")));
+				metadata.append(String.format("\"name\": \"%s\",", properties.get("name")));
+				metadata.append("\"source_type\": \"NEXUS\"");
+				metadata.append("}");
+				
+				System.out.println("Metadata: " +metadata.toString());
+				
+				// Create tar.gz in temporary directory
+				final Path tmp = Paths.get("/tmp/nexus-upload", UUID.randomUUID().toString());
+				final Path tarFile = Paths.get(tmp.toString(), tmp.getFileName().toString() +".tar.gz");
+				final Path metadataPath = Paths.get(tmp.toString(), ".metadata.json");
+				final Path artifactPath = Paths.get(tmp.toString(), artifact.getFileName().toString());
+				
+				Files.createDirectories(tmp);
+				Files.copy(artifact, artifactPath);
+				Files.write(metadataPath, metadata.toString().getBytes(Charset.forName("UTF-8")));
+
+				// Tarball.
+				try {
+					Runtime.getRuntime().exec(
+							new String[] {"tar", "cvzf", tarFile.toString(),
+									metadataPath.getFileName().toString(),
+									artifactPath.getFileName().toString()},
+							new String[] {},
+							tmp.toFile()).waitFor();
+				} catch (InterruptedException e) {
+					e.printStackTrace(); // FIXME: Handle better.
+				}
+				
+				// Post to gateway
+				pushToGeneric(tmp, tmp.getFileName().toString(), properties);
+				
+				// Cleanup
+//				Files.delete(Paths.get(tmp.toString(), ".metadata.json"));
+//				Files.delete(Paths.get(tmp.toString(), artifact.getFileName().toString()));
+//				Files.delete(Paths.get(tmp.toString(), tarFile.toString()));
+//				Files.delete(tmp);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		} else {
+			System.out.println("Ignoring artifact destined for " +repository +" repository. Not found in +" +Joiner.on(",").join(repositories));
+		}
+	
 	}
 
 	public void init(final FilterConfig config) throws ServletException {
@@ -173,12 +214,12 @@ public class OnUploadWebFilter implements Filter {
 	public void destroy() {
 	}
 	
-	private boolean notEndsWithAny(final String target, final String[] extensions) {
+	private boolean endsWithAny(final String target, final String[] extensions) {
 		for (final String extension : extensions) {
-			if (target.endsWith(extension)) return false;
+			if (target.endsWith(extension)) return true;
 		}
 		
-		return true;
+		return false;
 	}
 	
 	private void pushToGeneric(final java.nio.file.Path quarantinePath,
