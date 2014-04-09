1e2737368a75ce588ba16d78f97ff6bc602facf9
simonw2 <simon.white@gmail.com>
2014-04-09 16:19:33 +0100

    Missing import

--- src/main/java/com/surevine/community/nexus/OnManualUploadWebFilter.java
+++ src/main/java/com/surevine/community/nexus/OnManualUploadWebFilter.java
@@ -2,6 +2,7 @@ package com.surevine.community.nexus;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.Enumeration;
 
 import javax.inject.Singleton;
